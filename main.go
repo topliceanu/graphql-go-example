b336135fe05a3fec3f574e054d90e0400ed34e5d
alexandru topliceanu <alexandru.topliceanu@gmail.com>
2017-01-04 20:32:08 +0000

    add dependencies boilerplate and main.go

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	_ "github.com/lib/pq"
+)
+
+func handler(schema graphql.Schema) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		query, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		result := graphql.Do(graphql.Params{
+			Schema:        schema,
+			RequestString: string(query),
+		})
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(result)
+	}
+}
+
+var db *sql.DB
+
+func main() {
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query:    QueryType,
+		Mutation: MutationType,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	db, err = sql.Open("postgres", "postgres://vagrant:vagrant@localhost:5432/graphql")
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.Handle("/graphql", handler(schema))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
